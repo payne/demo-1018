7516de2c1a0940e5569127b94b9c2ff3b06d1e4d
Matt Payne <Payne@MattPayne.org>
2023-10-20 00:59:33 +0000

    hello world test added.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// import for @Ignore
+//import org.junit.Ignore;
 
+// annotation to ignore this test
+//@Ignore
+// annotation to not run this test
+@Disabled
 @SpringBootTest
 class DemoApplicationTests {
 
---
+++ src/test/java/com/example/demo/FunTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+// import for assertEquals
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FunTests {
+
+    @Test
+    void test1() {
+        assertEquals(1,1);
+    }
+
+}
